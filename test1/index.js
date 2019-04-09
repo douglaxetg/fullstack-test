b05e5188740d730b13e91546675d92975d728ca7
douglaxetg <douglastovar.g@.com>
2019-04-09 05:36:07 -0400

    se hicieron cambios en el llamdo de las funciones y se a#adio el metodo res.header en ambos enrutamiento para permitir a cualquier origen el acceso al recurso

--- test1/index.js
+++ test1/index.js
@@ -7,17 +7,20 @@ app.get('/', function (req, res) {
 });
 
 var users = require('./js/users');
-let getUserList = users.userList();
-let getUserDetails = users.userDetails(5);
 
 // userList page route
 app.get('/userlist', function (req, res) {
+  let getUserList = users.userList();
+  res.header("Access-Control-Allow-Origin", "*");
   res.send(getUserList);
 })
 
 // userDetails page route
-app.get('/userDetails', function (req, res) {
+app.get('/userdetails/:id', function (req, res) {
+  var id = req.params.id;
+  let getUserDetails = users.userDetails(id);
+  res.header("Access-Control-Allow-Origin", "*");
   res.send(getUserDetails);
 })
 
-app.listen(3000);
+app.listen(3001);
